058077e0ca84f0f50254a530ec85f2029e7e55c1
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-19 18:10:49 -0400

    Update gameplay.ts

--- Decentraland/src/gameplay.ts
+++ Decentraland/src/gameplay.ts
@@ -24,6 +24,10 @@ function runSendmessage(){
   //sendMessageEntity.addComponent(new Transform())
 
 // add a repeated function
+// add a repeated function
+//需要提高吗？
+//every n milliseconds.
+//0.5 second
   sendMessageEntity.addComponent(
   new utils.Interval(500, () => {
 room.send("player-position", Camera.instance.feetPosition);
