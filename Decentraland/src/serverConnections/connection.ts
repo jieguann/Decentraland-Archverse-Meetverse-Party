e6e4b52d16ea547e47989dfb05d28be9eaeeece8
jieguann <60665347+jieguann@users.noreply.github.com>
2021-11-15 18:34:16 -0500

    update server address

--- Decentraland/src/serverConnections/connection.ts
+++ Decentraland/src/serverConnections/connection.ts
@@ -31,7 +31,7 @@ export async function connect(roomName: string, options: any = {}) {
     // const ENDPOINT = "wss://hept-j.colyseus.dev";
 
 
-    const ENDPOINT = (isPreview) ? "ws://127.0.0.1:2567" : "wss://ymyzx8.colyseus.dev";
+    const ENDPOINT = (isPreview) ? "ws://127.0.0.1:2567" : "wss://noc8vp.us-east-vin.colyseus.net";
     // const ENDPOINT =  "wss://ymyzx8.colyseus.dev" // local environment
 
 
