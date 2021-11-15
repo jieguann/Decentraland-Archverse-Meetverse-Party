4dadf42d456f08bd8bbc6bd58c06f7dd2a4c0352
yaofan <yaofan0801@163.com>
2021-11-16 07:20:51 +0800

    修改firebase认证

--- colyseusServer/lib/rooms/MyRoom.js
+++ colyseusServer/lib/rooms/MyRoom.js
@@ -6,11 +6,14 @@
 方案二：
 实时把array发送到客户端
 */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MyRoom = exports.ArrayPlayersPosition = void 0;
 const colyseus_1 = require("colyseus");
 const MyRoomState_1 = require("./schema/MyRoomState");
-// import DB from '../db';
+const db_1 = __importDefault(require("../db"));
 //let midPlayer
 exports.ArrayPlayersPosition = [];
 //let ArrayPlayersPosition2:any[] = []
@@ -38,12 +41,17 @@ class MyRoom extends colyseus_1.Room {
         });
         this.onMessage("record-timeline", (client, recordInfo = {}) => {
             let { date, music, position } = recordInfo;
-            position = position ? JSON.stringify(position, ['x', 'y', 'z', 'sessionId']) : "";
-            // DB.run(`insert into timeline(date, music, position) values("${date}", "${music}", '${position}')`, function (err: any) {
-            //   if(err) {
-            //     console.log(err);
-            //   }
-            // })
+            position = position ? JSON.stringify(position) : "";
+            try {
+                db_1.default.doc('/' + Math.floor(Math.random() * 1000000) + '/').set({
+                    "date": date,
+                    "music": music,
+                    "position": position
+                });
+            }
+            catch (error) {
+                console.log(error);
+            }
         });
         //get osc message
         this.onMessage('change', (client, message) => {
